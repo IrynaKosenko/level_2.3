88420d0f74a9eecf906e6b3321211e2d86ce362c
Iryna_Kosenko <101429786+IrynaKosenko@users.noreply.github.com>
2023-06-12 22:17:59 +0300

    Update server.ts

--- HTTP/server.ts
+++ HTTP/server.ts
@@ -1,1 +1,36 @@
+import http from 'http';
+import querystring from 'querystring';
+import ip from 'ip';
 
+const PORT = 3000;
+const server = http.createServer().listen(PORT);
+
+server.on('connection', (socket) => {
+  console.log('\nStart session at ' + new Date().toUTCString());
+  console.log(socket.localAddress);
+  console.log(ip.address());
+});
+
+server.on('request', (req, res) => {
+  let start = Date.now();
+  let body: string;
+  if (req.method == 'POST') {
+    body = '';
+  }
+  req.on('data', (d) => {
+    body += d;
+  });
+  req.on('end', () => {
+    let post = querystring.parse(body);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(post.msg);
+    console.log(`Time: ${Date.now() - start} ms`);
+    console.log('closing the session at ' + new Date().toUTCString());
+  });
+
+  req.on('error', (e) => { 
+    console.log(e.message);
+  })
+})
+
+console.log('Listening on port ' + PORT);
